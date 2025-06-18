24205946f7e1479346a5ed37259055b2afffdaea
Paulo Santos <ph744915@gmail.com>
2025-06-18 17:14:32 -0300

    icones type pokemon

--- js/main.js
+++ js/main.js
@@ -1,42 +1,59 @@
 const pokedex = document.getElementById('pokedex');
 
 async function fetchPokemon(id) {
-    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-    const res = await fetch(url);
-    return await res.json();
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await fetch(url);
+  return await res.json();
+}
+
+function getIcon(type) {
+  return `./img/icons/${type}.svg`;
 }
 
 async function loadPokemon() {
-    for (let i = 1; i <= 151; i++) {
-        const pokemon = await fetchPokemon(i);
-        const name = pokemon.name;
-        const id = pokemon.id;
+  for (let i = 1; i <= 151; i++) {
+    const pokemon = await fetchPokemon(i);
+    const name = pokemon.name;
+    const id = pokemon.id;
 
-        let type = "";
-        for (let j = 0; j < pokemon.types.length; j++) {
-            const typeName = pokemon.types[j].type.name;
-            type += typeName;
+    let typeNames = [];
+    let typeHTML = "";
 
-            if (j < pokemon.types.length - 1) {
-                type += ", ";
-            }
-        }
+    for (let j = 0; j < pokemon.types.length; j++) {
+      const typeName = pokemon.types[j].type.name;
+      typeNames.push(typeName);
 
-        const col = document.createElement('div');
-        col.className = 'col-6 col-md-4 col-lg-2 mb-4';
+      const iconType = getIcon(typeName);
 
-        col.innerHTML = `
-          <figure class="card pokemon-card h-100 text-center">
-            <img src="${pokemon.sprites.front_default}" alt="${name}" class="pokemon-img p-3">
-            <figcaption class="card-body">
-              <h6 class="card-title mb-1 pokemon-title">${name} #${id}</h6>
-              <p class="card-text text-muted"><small>${type}</small></p>
-            </figcaption>
-          </figure>
-        `;
+      typeHTML += /*html*/`
+        <div class="d-flex align-items-center mx-1">
+          <img src="${iconType}" alt="${typeName}" title="${typeName}" class="me-1 type-icon ${typeName}">
+          <small class="text-capitalize">${typeName}</small>
+        </div>
+      `;
 
-        pokedex.appendChild(col);
+
     }
+
+    const col = document.createElement('div');
+    col.className = 'col-6 col-md-4 col-lg-2 mb-4';
+
+    col.innerHTML = /*html*/`
+      <figure class="card pokemon-card text-center h-100">
+        <img src="${pokemon.sprites.front_default}" alt="${name}" class="pokemon-img p-3">
+        <figcaption class="card-body">
+          <h6 class="card-title mb-1 pokemon-title">${name} #${id}</h6>
+          <div class="d-flex justify-content-center flex-wrap">
+            ${typeHTML}
+          </div>
+        </figcaption>
+      </figure>
+      `;
+
+    pokedex.appendChild(col);
+  }
+
+
 }
 
 loadPokemon();
