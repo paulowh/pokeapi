4a86129a9ef56b88144edf515ccd8885fbc6e8f8
Paulo <ph744915@gmail.com>
2025-07-24 00:41:34 -0300

    removendo na tela de iphone

--- template/header.php
+++ template/header.php
@@ -18,7 +18,8 @@ require_once __DIR__ . '/../function.php';
 
     <!-- Meta Tags para iPhone -->
     <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+    <meta name="format-detection" content="telephone=no">
     <meta name="apple-mobile-web-app-title" content="Detonado Pokémon">
     <meta name="theme-color" content="#345952">
 
